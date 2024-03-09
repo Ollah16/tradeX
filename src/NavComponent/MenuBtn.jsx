c2937c05b4b2441e44d48a960821854fec5e77b2
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-03-09 10:26:03 -0500

    navDropSwitch

--- src/NavComponent/MenuBtn.jsx
+++ src/NavComponent/MenuBtn.jsx
@@ -5,7 +5,7 @@ const MenuBtn = (props) => {
     const { handleDrop, navDrop } = props
 
     return (
-        <div className='md:hidden py-5 h-full mb-[-5px]'>
+        <div className='md:hidden py-5 h-full'>
             <MenuIcon onClick={() => handleDrop(!navDrop)} className='text-blue-200 h-7 w-7 cursor-pointer' />
         </div>
     )
--- src/NavComponent/NavCloseButton.jsx
+++ src/NavComponent/NavCloseButton.jsx
@@ -6,7 +6,7 @@ const NavCloseButton = ({ handleDrop, navDrop }) => {
         <div className={`${navDrop ? 'left-[-20px]' : '-left-[500px]'} flex justify-center 
         transition-left ease-in-out duration-500 md:hidden 
         items-center mr-5 absolute top-0 h-full flex-col bg-gray-900 w-14`}>
-            <XIcon onClick={() => handleDrop(!navDrop)} className='text-blue-200 h-7 w-7 cursor-pointer mb-[-5px]' />
+            <XIcon onClick={() => handleDrop(!navDrop)} className='text-blue-200 h-7 w-7 cursor-pointer' />
         </div>
     )
 }
