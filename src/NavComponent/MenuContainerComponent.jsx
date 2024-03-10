0be11859debe5f4cafa90a8f9fc272856c271152
Ollah16 <olaoluwaboluwatife@yahoo.com>
2024-03-10 10:22:22 -0400

    lineUpdate

--- src/NavComponent/MenuContainerComponent.jsx
+++ src/NavComponent/MenuContainerComponent.jsx
@@ -6,7 +6,7 @@ const MenuContainerComponent = ({ menuRef, menuHover, menuList }) => {
 
 
     return (
-        <div className={`bg-white text-black mx-10 md:fixed hidden z-20 transition-top ease-in-out duration-500 lg:block ${menuHover ? 'md:top-[75px]' : 'md:top-[-100%]'}`} ref={menuRef}>
+        <div className={`bg-white text-black mx-10 md:fixed hidden z-20 transition-top ease-in-out duration-500 md:block ${menuHover ? 'md:top-[75px]' : 'md:top-[-100%]'}`} ref={menuRef}>
             <NavMenuContent menuList={menuList} />
         </div>
     )
